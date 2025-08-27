f715c0e883a2c0de9ddc73e3d2b46fa6783edb92
Aqueel Ahamed Ali <99889444+Aqueel-Ahamed-Ali@users.noreply.github.com>
2025-08-27 20:35:02 +0530

    Update house_robber_2.py
    i have missed to update len(nums)==1 what should we do 
    we need to avoid the error in this code so retuntheir num[0] value is must otherwise it will show error message

--- 213. House Robber ||/house_robber_2.py
+++ 213. House Robber ||/house_robber_2.py
@@ -1,5 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
         def robber(nums):
             if len(nums)==1:
                 return nums[0]
