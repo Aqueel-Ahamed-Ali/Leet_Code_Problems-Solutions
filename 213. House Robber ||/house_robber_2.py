f0667b8f970ca6d55bbeedbff8be887b7a8a7c33
Aqueel Ahamed Ali <99889444+Aqueel-Ahamed-Ali@users.noreply.github.com>
2025-08-27 20:25:28 +0530

    Create house_robber_2.py

---
+++ 213. House Robber ||/house_robber_2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def robber(nums):
+            if len(nums)==1:
+                return nums[0]
+            dp=[0]*len(nums)
+            dp[0]=nums[0]
+            dp[1]=max(nums[0],nums[1])
+            for i in range(2,len(nums)):
+                dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+            return dp[-1]
+        nums1=nums[1:len(nums)]
+        nums2=nums[0:-1]
+        m1=robber(nums1)
+        m2=robber(nums2)
+        return max(m1,m2)
